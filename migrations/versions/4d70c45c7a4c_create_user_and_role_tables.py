4e88a9727d77a3de48777c7607e486d32350f1bd
Unique Mlungu <uniquemlungu04@gmail.com>
2026-08-25 08:46:19 +0300

    Fix password column length to 255

--- migrations/versions/4d70c45c7a4c_create_user_and_role_tables.py
+++ migrations/versions/4d70c45c7a4c_create_user_and_role_tables.py
@@ -28,7 +28,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('email', sa.String(length=100), nullable=False),
-    sa.Column('password', sa.String(length=100), nullable=False),
+    sa.Column('password', sa.String(length=255), nullable=False),
     sa.Column('role_id', sa.Integer(), nullable=True),
     sa.Column('active', sa.Boolean(), nullable=True),
     sa.Column('create_at', sa.DateTime(), nullable=True),
